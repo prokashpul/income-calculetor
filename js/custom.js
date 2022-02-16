904dd8c1b232d339c5b7aa2746e63c0dcd3f4845
Prokash Pul <pprokash102@gmail.com>
2022-02-17 00:37:10 +0600

    error handaling add function

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@ function expensesCalculate() {
     if (foodInputValue >= 0 && rentInputValue >= 0 & clotheInputValue >= 0) {
         totalExpenses = foodInputValue + rentInputValue + clotheInputValue;
     } else {
-        document.getElementById('error').innerHTML = `Some thing error`
+        errorHandel("Expenses", " Negative number and 0 is not allow ");
     }
     // return function
     return totalExpenses;
@@ -22,12 +22,33 @@ function totalBalance() {
     const incomeInput = document.getElementById('income-input');
     const incomeInputValue = parseInt(incomeInput.value);
     let totalBalance = 0;
-    if (incomeInputValue >= expensesCalculate()) {
-        totalBalance = incomeInputValue - parseInt(expensesCalculate());
+    if (incomeInputValue > 0) {
+        if (incomeInputValue >= expensesCalculate()) {
+            totalBalance = incomeInputValue - parseInt(expensesCalculate());
+        } else {
+            errorHandel(expensesCalculate(), ` Is bigger than ${incomeInputValue} `);
+        }
+
     } else {
-        document.getElementById('error').innerHTML = ` ${expensesCalculate()} Expenses can't bigger than Income !`;
+        errorHandel("Income", " Negative number and 0 is not allow ");
     }
+
     return totalBalance;
+}
+// error handling function create
+function errorHandel(name, massage) {
+    document.getElementById('error').innerHTML = `<div class="toast show align-items-center my-3 mx-auto bg-danger text-light" role="alert" aria-live="assertive" aria-atomic="true">
+  <div class="d-flex">
+    <div class="toast-body">
+    ${name} input: 
+    Wrang Data ! ${massage} ! please enter 0 up any number;
+   </div>
+    <button type="button" id='close' class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+  </div>
+</div> `;
+    document.getElementById('close').addEventListener('click', function () {
+        document.getElementById('error').innerHTML = "";
+    })
 }
 
 // calculate button click event add
@@ -49,7 +70,7 @@ document.getElementById('save-btn').addEventListener('click', function () {
     const savingBalance = document.getElementById('saving-balance');
 
     // saving balance
-    savingBalance.innerText = Math.round(parseInt(totalBalance()) / saveInputValue);
+    savingBalance.innerText = Math.round(parseInt(totalBalance()) * saveInputValue / 100);
     const totalSave = parseInt(savingBalance.innerText);
     // Remaining Balance
     const remainingBalance = document.getElementById('remaining-balance');
