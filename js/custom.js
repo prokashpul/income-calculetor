5586698e2490b0c56f7c4ed63b55c615394a5b41
Prokash Pul <pprokash102@gmail.com>
2022-02-17 01:22:18 +0600

    error hadaling update

--- js/custom.js
+++ js/custom.js
@@ -7,13 +7,14 @@ function expensesCalculate() {
     const clotheInput = document.getElementById('clothe-input');
     const clotheInputValue = parseInt(clotheInput.value);
     let totalExpenses = 0;
-    if (foodInputValue >= 0 && rentInputValue >= 0 & clotheInputValue >= 0) {
+    if (foodInputValue >= 0 && rentInputValue >= 0 && clotheInputValue >= 0) {
         totalExpenses = foodInputValue + rentInputValue + clotheInputValue;
     } else {
         errorHandel("Expenses", " Negative number and 0 is not allow ");
     }
-    // return function
+    // return totalExpenses
     return totalExpenses;
+
 
 
 }
@@ -26,22 +27,22 @@ function totalBalance() {
         if (incomeInputValue >= expensesCalculate()) {
             totalBalance = incomeInputValue - parseInt(expensesCalculate());
         } else {
-            errorHandel(expensesCalculate(), ` Is bigger than ${incomeInputValue} `);
+            errorHandel(expensesCalculate(), `expenses Is bigger than income it's not allow`);
         }
-
     } else {
-        errorHandel("Income", " Negative number and 0 is not allow ");
+        errorHandel("Income", " Negative number and 0 and NuN data is not allow ! please enter 0 up any number ");
     }
-
+    // return  totalBalance
     return totalBalance;
 }
+
 // error handling function create
 function errorHandel(name, massage) {
     document.getElementById('error').innerHTML = `<div class="toast show align-items-center my-3 mx-auto bg-danger text-light" role="alert" aria-live="assertive" aria-atomic="true">
   <div class="d-flex">
     <div class="toast-body">
     ${name} input: 
-    Wrang Data ! ${massage} ! please enter 0 up any number;
+    Wrang Data😒 ! ${massage}!;
    </div>
     <button type="button" id='close' class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
   </div>
@@ -55,24 +56,32 @@ function errorHandel(name, massage) {
 
 document.getElementById('calculate-but').addEventListener('click', function () {
     // total Expenses show display
-    const totalExpenses = document.getElementById('total-expenses');
-    totalExpenses.innerText = expensesCalculate();
+    if (totalBalance() >= expensesCalculate()) {
+        const totalExpenses = document.getElementById('total-expenses');
+        totalExpenses.innerText = expensesCalculate();
+    }
     // balance function
     const balance = document.getElementById('balance');
     balance.innerText = totalBalance();
 
 });
 
-// saving button add event handler
+// saving button add event handler 
 document.getElementById('save-btn').addEventListener('click', function () {
     const saveInput = document.getElementById('save-input');
     const saveInputValue = parseInt(saveInput.value);
     const savingBalance = document.getElementById('saving-balance');
 
-    // saving balance
-    savingBalance.innerText = Math.round(parseInt(totalBalance()) * saveInputValue / 100);
-    const totalSave = parseInt(savingBalance.innerText);
-    // Remaining Balance
-    const remainingBalance = document.getElementById('remaining-balance');
-    remainingBalance.innerText = Math.round(parseInt(totalBalance()) - totalSave);
+    // saving balance percent value calculate
+    if (saveInputValue < 100 && saveInputValue >= 0) {
+
+        savingBalance.innerText = parseInt(totalBalance()) * saveInputValue / 100;
+        const totalSave = parseFloat(savingBalance.innerText).toFixed(2);
+        // Remaining Balance
+        const remainingBalance = document.getElementById('remaining-balance');
+        remainingBalance.innerText = Math.round(parseInt(totalBalance()) - totalSave);
+    } else {
+        errorHandel(saveInputValue, " up to 100 % / Negative number / NuN is not allow, please try again")
+    }
+
 });
